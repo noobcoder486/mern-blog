57db1dbd098bdce176580b321166f99460f0af5f
your.user.name <prathameshkokane@gmail.com>
2024-07-02 01:29:07 +0530

    MONGO DB Connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+dotenv.config()
 
 const app = express()
+
+mongoose.connect(process.env.MONGO).then(
+    ()=>{console.log("Mongo DB is Connect hogayi")}
+).catch((err)=>{console.log(err)})
+
 
 
 app.listen(3000, ()=>{
