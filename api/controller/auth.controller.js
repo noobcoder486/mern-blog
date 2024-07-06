ca07b954525971bcaa32684537e5a3d02566c338
your.user.name <prathameshkokane@gmail.com>
2024-07-07 01:15:12 +0530

    Added a middle  ware and error handler

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,13 +1,15 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
+import {errorHandler}  from '../utils/error.js'
 
-export const signup = async (req,res)=>
+export const signup = async (req,res,next)=>
 {
     const {username,email,password} = req.body
 
     if(!username || !email || !password || username === '' || email === '' || password === '')
     {
-        return res.status(500).json({message:'All fields are required'});
+        next(errorHandler(400,'All the fields are required'))
+        return
     }
 
     const hashPassword = bcryptjs.hashSync(password,10)
@@ -22,7 +24,6 @@ export const signup = async (req,res)=>
         await newUser.save()
         res.status(200).json({message:"Sign Up Successfull"})
     } catch (error) {
-        res.status(500).json({message:error.message.toString()})
-        
+        next(error)        
     }
 }
--- api/index.js
+++ api/index.js
@@ -3,10 +3,11 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import userRoute from './routes/user.routes.js'
 import authRoute from './routes/auth.routes.js'
+
+
 dotenv.config()
 
 const app = express()
-
 
 app.use(express.json())
 
@@ -22,3 +23,14 @@ app.listen(3000, ()=>{
 
 app.use('/api/user',userRoute)
 app.use('/api/auth',authRoute)
+
+app.use((err,req,res,next)=>
+{
+    const statusCode = err.statusCode || 500;
+    const errorMessage = err.message || 'Internal Server Error'
+    res.status(statusCode).json({
+        success:false,
+        statusCode : statusCode,
+        message:errorMessage
+    })
+})
